e1307ac4fcb3bed52809ec0918b8c33add98c6f1
sparkle666 <sikky606@gmail.com>
2024-01-03 21:01:58 +0100

    fix: added the render url to the ALLOWED HOST in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0",
-                 "127.0.0.1", "projectpro-ohsu.onrender.com"]
+                 "127.0.0.1", "vehicleobs.onrender.com"]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = [
